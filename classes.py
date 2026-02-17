a7942a636a7ab050f033c26e8cc91d1a448856c8
Hinch08 <hinchjack08@gmail.com>
2026-02-17 15:47:16 +0300

    MY DUKA

---
+++ classes.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self,name,age,email):
+        self.name = name
+        self.age = age
+        self.email = email
+
+person1 = Person("Hillary",30,"hinchjack08@gmail.com")
+print(type(person1))
+print(person1.name)
+print(person1.age)
+print(person1.email)
+        
+    
