5e2093fa38514692d4bd59bf88e820a03aa416bd
Hinch08 <hinchjack08@gmail.com>
2026-02-17 15:49:16 +0300

    MY DUKA

--- classes.py
+++ classes.py
@@ -9,5 +9,11 @@ print(type(person1))
 print(person1.name)
 print(person1.age)
 print(person1.email)
+person2 = Person("John",25,"johnmaina@gmail.com")
+print(type(person2))
+print(person2.name)
+print(person2.age)
+print(person2.email)
+
         
     
