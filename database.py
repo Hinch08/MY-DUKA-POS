d9c369a33493dfc2e7e56bc1d407597c24b6acc2
Hinch08 <hinchjack08@gmail.com>
2026-02-24 16:03:38 +0300

    MY DUKA

--- database.py
+++ database.py
@@ -12,48 +12,48 @@ def get_data(tables):
  data = cur.fetchall()
  return data
 
-data = get_data("sales")
-print(data)
+# data = get_data("sales")
+# print(data)
 
 def get_products():
  cur.execute("select * from products")
  products = cur.fetchall()
  return products
 
-products = get_products()
-print(products)
+# products = get_products()
+# print(products)
 
 
 def insert_products(values):
     cur.execute(f"insert into products(name,buying_price,selling_price)values{values}")
     conn.commit()
     
-product1 = ('samsung',20000,30000)
-product2 = ('hp',30000,40000)
+# product1 = ('samsung',20000,30000)
+# product2 = ('hp',30000,40000)
 
-insert_products(product1)
-insert_products(product2)
+# insert_products(product1)
+# insert_products(product2)
 
 def fetch_sales():
     cur.execute("select * from sales")
     sales = cur.fetchall()
     return sales
 
-sales = fetch_sales()
-print(sales)
+# sales = fetch_sales()
+# print(sales)
 
 def insert_sale(values):
     cur.execute(f"insert into sales(pid,quantity)values{values}")
     conn.commit()
 
     
-sale1 = (3,30)
-sale2 = (4,12)
+# sale1 = (3,30)
+# sale2 = (4,12)
 
-insert_sale(sale1)
-insert_sale(sale2)
+# insert_sale(sale1)
+# insert_sale(sale2)
 
-print(sales)
+# print(sales)
 
 
 def get_sales_per_product():
@@ -64,8 +64,8 @@ def get_sales_per_product():
     sales_per_product = cur.fetchall()
     return sales_per_product
 
-sales_per_product = get_sales_per_product()
-print(sales_per_product)
+# sales_per_product = get_sales_per_product()
+# print(sales_per_product)
 
 
 
@@ -78,11 +78,8 @@ def get_profit_per_day():
     return profit_per_day
 
 
-profit_per_day = get_profit_per_day()
-print(profit_per_day)
+# profit_per_day = get_profit_per_day()
+# print(profit_per_day)
 
 # cur.close()
 # conn.close()
-
-
-
