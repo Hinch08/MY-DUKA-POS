0ee75a0ec6562e8c9be15fac6bac5180d145506f
Hinch08 <hinchjack08@gmail.com>
2026-02-26 15:42:33 +0300

    MY DUKA

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from database import get_products,fetch_sales
+from flask import Flask, render_template, request,redirect,url_for
+from database import get_products,fetch_sales,insert_products,insert_sale
 
 app = Flask(__name__)
 @app.route('/')
@@ -11,6 +11,19 @@ def products():
     products = get_products()
     return render_template('products.html', products = products)
 
+@app.route('/add_products',methods=['GET','POST'])
+def add_products():
+    if request.method == 'POST':
+        product_name = request.form['p_name']
+        buying_price = request.form['b_price']
+        selling_price = request.form['s_price']
+        new_product = (product_name,buying_price,selling_price)
+        insert_products(new_product)
+        print("Product added successfully")
+    return redirect(url_for('products'))
+
+
+
 @app.route('/sales')
 def sales():
     sales = fetch_sales()
@@ -18,7 +31,9 @@ def sales():
 
 @app.route('/stock')
 def stock():
-    return render_template('stock.html')
+    value = 789
+    numbers = [1,2,3,4,5,6,7,8,9]
+    return render_template('stock.html',x=value,y=numbers)
 
 @app.route('/dashboard')
 def dashboard():
